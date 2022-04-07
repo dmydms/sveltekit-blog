7c01a5952ba7e4070234292c574348a39fb639f0
dmydms <dahyun@kakao.com>
2022-04-07 15:39:48 +0900

    feat: index page 레이아웃 추가

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import type { ArticleMeta } from '$lib/types'
+
+    export let posts: ArticleMeta[] = [] 
+</script>
+
+<svelte:head>
+    <title>웹사이트 제목</title>
+    <meta name="description" content="웹사이트 설명" />
+</svelte:head>
+
+<h1>웹사이트 제목</h1>
+
+<ul class="posts">
+    {#each posts as post}
+        <li>
+            <article>
+                <h2 class="title"><a href={`/posts/${post.id}`}>{post.title}</a></h2>
+                <time class="published-at">{post.publishedAt}</time>
+                <p class="summary">{post.summary}</p>
+            </article>
+        </li>
+    {/each}
+</ul>
