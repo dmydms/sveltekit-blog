12c67bf13a749878f6a8e708560fee2549756e86
dmydms <dahyun@kakao.com>
2022-04-07 18:28:17 +0900

    refactor: 정렬 수정

--- src/components/LayoutDefault.svelte
+++ src/components/LayoutDefault.svelte
@@ -1,24 +1,24 @@
 <script>
-    export let title = '';
-    export let sumarry = '';
-    export let publishedAt = '';
-</script>
-
-<svelte:head>
+    export let title = ''
+    export let summary = ''
+    export let publishedAt = ''
+  </script>
+  
+  <svelte:head>
     <title>{title}</title>
-    <meta name="description" content={sumarry} />
-</svelte:head>
-
-<article class="post">
+    <meta name="description" content={summary} />
+  </svelte:head>
+  
+  <article class="post">
     <h1>{title}</h1>
     <p class="info">
-        <time>{publishedAt}</time>
+      <time>{publishedAt}</time>
     </p>
-    <slot/>
-</article>
-
-<style>
+    <slot />
+  </article>
+  
+  <style>
     .info {
-        margin: 1em 0 6em;
+      margin: 1em 0 6em;
     }
-</style>
+  </style>
