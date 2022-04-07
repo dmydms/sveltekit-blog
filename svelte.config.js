ed3f7b46b534330fa8f70f5a57dd222632f3744a
dmydms <dahyun@kakao.com>
2022-04-08 00:49:05 +0900

    각종 셋팅 추가

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,26 @@
-import { fileURLToPath } from 'url'
-import { mdsvex } from 'mdsvex'
 import adapter from '@sveltejs/adapter-static'
+import { mdsvex } from 'mdsvex'
 import path from 'path'
-import preprocess from 'svelte-preprocess'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeSlug from 'rehype-slug'
 import remarkAbbr from 'remark-abbr'
+import preprocess from 'svelte-preprocess'
+import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
   extensions: ['.svelte', '.md'],
   preprocess: [
-    preprocess(),
+    preprocess({
+      postcss: {
+        configFilePath: path.join(__dirname, 'postcss.config.js'),
+      },
+    }),
     mdsvex({
       extensions: ['.md'],
       smartypants: {
@@ -27,11 +33,24 @@ const config = {
       },
     }),
   ],
+
   kit: {
     adapter: adapter(),
-	prerender: {
-		default: true,
-	}
+    prerender: {
+      default: true,
+    },
+    vite: {
+      resolve: {
+        alias: {
+          $components: path.resolve('./src/components'),
+        },
+      },
+      test: {
+        globals: true,
+        environment: 'jsdom',
+      },
+    },
+    trailingSlash: 'ignore',
   },
 }
 
