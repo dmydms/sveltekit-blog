062acff2c7b8c01800295d0eadeb189b6ea122e1
dmydms <dahyun@kakao.com>
2022-04-07 14:23:31 +0900

    chore: 공백 수정 및 `adapter-static`으로 변경

--- svelte.config.js
+++ svelte.config.js
@@ -1,36 +1,35 @@
 import { fileURLToPath } from 'url'
-import { mdsvelx } from 'mdsvex'
-import adapter from '@sveltejs/adapter-auto';
+import { mdsvex } from 'mdsvex'
+import adapter from '@sveltejs/adapter-static'
 import path from 'path'
-import preprocess from 'svelte-preprocess';
+import preprocess from 'svelte-preprocess'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeSlug from 'rehype-slug'
 import remarkAbbr from 'remark-abbr'
-
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md'],
-	preprocess: [
-		preprocess(),
-		mdsvex({
-			extensions: ['.md'],
-			smartypants: {
-				dashes: 'oldschool',
-			},
-			remarkPlugins: [remarkAbbr],
-			rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {behavior: 'wrap'}]],
-			layout: {
-				_: path.join(__dirname, './src/components/LayoutDefault.svelte'),
-			}
-		})
-	],
-	kit: {
-		adapter: adapter()
-	}
-};
+  extensions: ['.svelte', '.md'],
+  preprocess: [
+    preprocess(),
+    mdsvex({
+      extensions: ['.md'],
+      smartypants: {
+        dashes: 'oldschool',
+      },
+      remarkPlugins: [remarkAbbr],
+      rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap' }]],
+      layout: {
+        _: path.join(__dirname, './src/components/LayoutDefault.svelte'),
+      },
+    }),
+  ],
+  kit: {
+    adapter: adapter(),
+  },
+}
 
-export default config;
+export default config
