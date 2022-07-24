2c38a10ae84add1b1935ef35a2aebf8cb8ac678e
sunny <sunny@zent.kr>
2022-07-25 00:48:59 +0900

    feat: 메인 컨텐츠 추가 및 CSS 적용

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,32 +1,107 @@
-<script lang="ts">
-    import type { ArticleMeta } from '$lib/types'
-
-    export let posts: ArticleMeta[] = []
+<script>
+  import profileImg from '$lib/assets/profile.png'
 </script>
 
 <svelte:head>
   <!-- Google Tag Manager -->
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-NW5PX94');</script>
+  <script>
+    ;(function (w, d, s, l, i) {
+      w[l] = w[l] || []
+      w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' })
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : ''
+      j.async = true
+      j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
+      f.parentNode.insertBefore(j, f)
+    })(window, document, 'script', 'dataLayer', 'GTM-NW5PX94')
+  </script>
   <!-- End Google Tag Manager -->
-    <title>개발하는 다현</title>
-    <meta name="description" content="안녕하세요! 제품 개발자 김다현입니다.🙋‍♀️" />
+  <title>개발하는 다현</title>
+  <meta name="description" content="Backend Engineer 김다현입니다." />
 </svelte:head>
 
-<h1>블로그 홈</h1>
-<p>생각, 회고, 개발 이야기 등을 기록합니다.</p>
+<div class="profile-wrapper">
+  <div class="profile-box">
+    <img class="profile" src={profileImg} alt={'프로필 사진'} />
+  </div>
+  <div class="title-desc">
+    <h1 class="title">김다현, <small>doomdoom</small></h1>
+    <p class="desc">
+      📍 백엔드 개발자에요. Nest.js를 주로 다룹니다. <br />
+      📍 음악을 좋아하고 노래를 즐겨불러요. <br />
+      📍 술을 좋아해요. 프로혼술러!<br />
+    </p>
+  </div>
+</div>
 
-<ul class="posts">
-    {#each posts as post}
-        <li>
-            <article>
-                <h2 class="title"><a href={`/posts/${post.id}`}>{post.title}</a></h2>
-                <time class="published-at">{post.publishedAt}</time>
-                <p class="summary">{post.summary}</p>
-            </article>
-        </li>
-    {/each}
+<h2 class="section-title">좋아하는 음악</h2>
+<p>양인모님의 Histoire du Tango: III. Nightclub 1960을 가장 좋아해요.</p>
+
+<h2 class="section-title">좋아하는 술</h2>
+<ul class="drinks">
+  <li>
+    <h3>
+      <a href="https://www.ardbeg.com/en-int/whisky/ultimate-range/ten-years-old">Ardbeq Ten</a>
+    </h3>
+    <p>
+      개인적으로 가다랑어포 향과 비슷하다고 느껴지는 피트향이 진한 위스키에요. 제가 좋아하는
+      바이올리니스트 양인모님의 연주를 들으면 이 술이 종종 떠오르곤 하더라구요.
+    </p>
+  </li>
+  <li>
+    <h3>
+      <a href="https://www.calvados-boulard.com/en/calvados/boulard-x-o/">Calvados Boulard XO</a>
+    </h3>
+    <p>
+      연희동의 <a href="https://www.instagram.com/chaegbar/">'책바'</a>라는 바에서 처음 마셔본
+      술이에요. 은은한 사과향이 무척 인상깊었던 술이랍니다. 늘 자극적이고 강렬한 무언가를 좋아하는
+      제게 은근한 맛의 매력을 알게 해준 술이에요.
+    </p>
+  </li>
 </ul>
+
+<style lang="postcss">
+  .profile-wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 2.5em 0;
+    gap: 2em;
+  }
+
+  .title {
+    margin: 0;
+  }
+
+  .desc {
+    margin-top: 0.25em;
+    margin-bottom: 0;
+    line-height: 2.5em;
+  }
+
+  .profile-box {
+    overflow: hidden;
+    width: 14em;
+    height: 14em;
+    border-radius: 100%;
+    box-shadow: 0 20px 13px rgb(0 0 0 / 3%);
+  }
+
+  .profile {
+    width: 100%;
+  }
+
+  .section-title {
+    margin-bottom: 0.2em;
+  }
+
+  .drinks {
+    h3 {
+      margin: 0 0 0.2em;
+    }
+
+    p {
+      margin-top: 0;
+    }
+  }
+</style>
