8c43387307149ee3e117af88e18a7eb578ec20ab
sunny <sunny@zent.kr>
2022-07-25 00:46:30 +0900

    feat: 에세이 탭 페이지 추가

---
+++ src/routes/posts/writing/index.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import PostList from '$components/PostList.svelte'
+  import type { ArticleMeta } from '$lib/types'
+
+  export let posts: ArticleMeta[] = []
+</script>
+
+<svelte:head>
+  <title>에세이</title>
+  <meta name="description" content="경험과 떠올랐던 생각들을 기록해요." />
+</svelte:head>
+
+<h1>에세이</h1>
+<p>경험과 떠올랐던 생각들을 기록해요.</p>
+
+<PostList dir={'writing'} {posts} />
+
+<style lang="postcss">
+  h1 {
+    margin-bottom: 0;
+  }
+
+  p {
+    margin-top: 0.25em;
+    margin-bottom: 0;
+  }
+</style>
